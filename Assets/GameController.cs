f0c197616888a87ebd346b05f48e8ff6335a0d21
Jun Shimura <junshimura@gmail.com>
2022-01-27 12:06:33 +0900

    ScoreText

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public enum GameState { Ready,InGame,GameOver}
+
+    [SerializeField]
+    ScoreText scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+}
---
+++ Assets/ScoreText.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+
+public class ScoreText : MonoBehaviour
+{
+    [SerializeField]
+    string scoreHeader = "SCORE:";
+    string scoreFomat = "D5";
+
+    private int _score = 0;
+
+    public int Score
+    {
+        set {
+            if (_score != value)
+            {
+                _score = value;
+                UpdateValue();
+            }
+        }
+    }
+
+    private Text uiText;
+    private void Awake()
+    {
+        uiText = GetComponent<Text>();
+        UpdateValue();
+    }
+    private void UpdateValue()
+    {
+        uiText.text = scoreHeader + _score.ToString(scoreFomat);
+    }
+}
